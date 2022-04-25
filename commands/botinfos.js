ce8473574ecf9e4dab833c3af8ff24fb06f79f4d
Mykelinos <100524762+Mykelinos@users.noreply.github.com>
2022-04-25 15:01:55 +0200

    Update botinfos.js

--- commands/botinfos.js
+++ commands/botinfos.js
@@ -5,11 +5,11 @@ const {
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('botinfo')
-    .setDescription('Gives the credits of the bot.'),
+    .setDescription('Dai i crediti del bot <3'),
   async execute(interaction, client) {
     const embed = new client.discord.MessageEmbed()
       .setColor('6d6ee8')
-      .setDescription('Developed with the 💜 by `•OofyOofOof•#2018`\n\n[`Github`](https://github.com/blackknight683) | [`Twitch`](https://www.twitch.tv/oofydaoofer) | [`Discord`](https://discord.gg/S2GGa23) | [`Youtube`](https://youtube.com/c/BlackKnight683)')
+      .setDescription('Developed by Mykelino')
       .setFooter(client.user.tag, client.user.avatarURL())
       .setTimestamp();
     await interaction.reply({
